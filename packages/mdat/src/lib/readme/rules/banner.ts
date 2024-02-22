cce9ae4975f355c2f1f8a77b33f9ca782f9dc8b3
Eric Mika <eric@ericmika.com>
2024-02-22 16:57:47 +0900

    Add support for remote URLs in banner rule.

--- packages/mdat/src/lib/readme/rules/banner.ts
+++ packages/mdat/src/lib/readme/rules/banner.ts
@@ -6,6 +6,52 @@ import { readPackage } from 'read-pkg'
 import type { Rules } from 'remark-mdat'
 import { z } from 'zod'
 
+export default {
+	banner: {
+		async content(options?) {
+			// Check options, throws if invalid
+			const validOptions = z
+				.object({
+					alt: z.string().optional(),
+					src: z.string().optional(),
+				})
+				.optional()
+				.parse(options)
+
+			const { assetsPath, packageFile } = await getConfig()
+			if (packageFile === undefined) {
+				throw new Error('No package.json found')
+			}
+
+			// Try to find the src in various places
+			// Don't do the deep search, it can find too many things
+			const src = validOptions?.src ?? (await getBannerSrc(assetsPath)) // ?? (await getBannerSrc())
+
+			if (src === undefined) {
+				throw new Error(
+					'Banner image not found at any typical location, consider adding something at ./assets/banner.webp',
+				)
+			} else if (!isUrl(src) && !(await isFile(src))) {
+				throw new Error(`Banner image not found at "${src}"`)
+			}
+
+			// Try to find the alt, otherwise derive it from the package name
+			const alt =
+				validOptions?.alt ??
+				// eslint-disable-next-line unicorn/no-await-expression-member
+				`${(await readPackage({ cwd: path.dirname(packageFile) })).name} banner`
+			if (alt === undefined || alt === 'undefined banner') {
+				throw new Error(`Banner image alt text not available`)
+			}
+
+			return `![${alt}](${src})`
+		},
+		order: 2,
+	},
+} satisfies Rules
+
+// Helpers
+
 async function getBannerSrc(specificPath?: string): Promise<string | undefined> {
 	// Check some typical locations
 	const { packageFile } = await getConfig()
@@ -53,43 +99,25 @@ async function getBannerSrc(specificPath?: string): Promise<string | undefined> 
 	return undefined
 }
 
-export default {
-	banner: {
-		async content(options?) {
-			// Check options, throws if invalid
-			const validOptions = z
-				.object({
-					alt: z.string().optional(),
-					src: z.string().optional(),
-				})
-				.optional()
-				.parse(options)
+// Via https://github.com/sindresorhus/is-url-superb
+function isUrl(text: string, lenient = true): boolean {
+	if (typeof text !== 'string') {
+		throw new TypeError('Expected a string')
+	}
 
-			const { assetsPath, packageFile } = await getConfig()
-			if (packageFile === undefined) {
-				throw new Error('No package.json found')
-			}
+	text = text.trim()
+	if (text.includes(' ')) {
+		return false
+	}
 
-			// Try to find the src in various places
-			// Don't do the deep search, it can find too many things
-			const src = validOptions?.src ?? (await getBannerSrc(assetsPath)) // ?? (await getBannerSrc())
-			if (src === undefined || !(await isFile(src))) {
-				throw new Error(
-					`Banner image not found at ${src === undefined ? 'any typical location, consider adding something at ./assets/banner.webp' : `"${src}"`}`,
-				)
-			}
+	try {
+		new URL(text) // eslint-disable-line no-new
+		return true
+	} catch {
+		if (lenient) {
+			return isUrl(`https://${text}`, false)
+		}
 
-			// Try to find the alt, otherwise derive it from the package name
-			const alt =
-				validOptions?.alt ??
-				// eslint-disable-next-line unicorn/no-await-expression-member
-				`${(await readPackage({ cwd: path.dirname(packageFile) })).name} banner`
-			if (alt === undefined || alt === 'undefined banner') {
-				throw new Error(`Banner image alt text not available`)
-			}
-
-			return `![${alt}](${src})`
-		},
-		order: 2,
-	},
-} satisfies Rules
+		return false
+	}
+}
