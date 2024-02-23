ce20524cad2c518c4fce46a7b7f470df217480d0
Eric Mika <eric@ericmika.com>
2024-02-23 21:17:39 +0900

    Dependency updates.

--- .remarkrc.js
+++ .remarkrc.js
@@ -3,13 +3,11 @@ import sharedConfig, { overrideRules } from '@kitschpatrol/remark-config'
 const localConfig = {
 	...sharedConfig,
 	plugins: overrideRules(sharedConfig.plugins, [
-		['remark-lint-no-missing-blank-lines', false],
+		// Exceptions for test markdown files
 		['remark-lint-first-heading-level', false],
 		['remark-lint-no-duplicate-headings', false],
 		['remark-lint-no-duplicate-headings-in-section', false],
-		['remark-lint-no-html', false],
 		['remark-lint-no-multiple-toplevel-headings', false],
-		['remark-lint-no-emphasis-as-heading', false],
 	]),
 }
 
