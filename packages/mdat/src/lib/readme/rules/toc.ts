6affb005d3ae473f5b4deb65a9552fdb159851e8
Eric Mika <eric@ericmika.com>
2024-02-29 13:36:32 -0500

    Fix <!-- toc --> alias option argument parsing.

--- packages/mdat/src/lib/readme/rules/toc.ts
+++ packages/mdat/src/lib/readme/rules/toc.ts
@@ -5,8 +5,5 @@ import { type Rules, getSoleRule } from 'remark-mdat'
  * Simple alias for table-of-contents
  */
 export default {
-	toc: {
-		applicationOrder: 1,
-		content: [getSoleRule(tableOfContents)],
-	},
+	toc: getSoleRule(tableOfContents),
 } satisfies Rules
