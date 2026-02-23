6149b6ef2ae03b9a51134e732120c5d6b7f50383
Eric Mika <eric@ericmika.com>
2026-02-23 13:13:29 -0500

    Fix makeTitleCase producing extra spaces on consecutive delimiters.

--- src/lib/readme/rules/title.ts
+++ src/lib/readme/rules/title.ts
@@ -27,7 +27,8 @@ export default {
 
 function makeTitleCase(text: string): string {
 	return text
-		.split(/[ _-]/)
+		.split(/[ _-]+/)
+		.filter(Boolean)
 		.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
 		.join(' ')
 }
