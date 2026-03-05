c51b45615039d7962658fc0347e771aa8b1c8a2d
Eric Mika <eric@ericmika.com>
2026-03-05 11:19:44 -0500

    Fix: Temporary work-around for jiti babel lazy loading issue.

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,17 +4,32 @@ import raw from 'unplugin-raw/rollup'
 export default defineConfig([
 	// CLI tool
 	{
+		deps: {
+			alwaysBundle: /.+/,
+			onlyAllowBundle: false,
+		},
 		dts: false,
 		entry: 'src/bin/cli.ts',
 		fixedExtension: false,
-		inlineOnly: false,
-		minify: true,
-		noExternal: /.+/,
+		minify: false,
 		outDir: 'dist/bin',
 		platform: 'node',
 		plugins: [
 			// Supports ?raw suffix from vite...
 			raw(),
+			// https://github.com/unjs/jiti/issues/417
+			// https://github.com/unjs/jiti/pull/430
+			{
+				name: 'jiti-babel-eager',
+				transform(code, id) {
+					if (!id.includes('jiti')) return
+					// Replace lazy createRequire(import.meta.url)(...) with direct require
+					return code.replace(
+						`createRequire(import.meta.url)("../dist/babel.cjs")`,
+						`require("../dist/babel.cjs")`,
+					)
+				},
+			},
 		],
 	},
 	// Library
