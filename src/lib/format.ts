89b6ba38385118ae2c3e030c2a1ae48faf4a57c5
Eric Mika <eric@ericmika.com>
2026-04-01 03:54:48 -0400

    Feature: Add format option to run output through prettier.

---
+++ src/lib/format.ts
@@ -0,0 +1,29 @@
+import type Prettier from 'prettier'
+import { log } from './log'
+
+/**
+ * Format a markdown string with Prettier, using config discovered from the file path.
+ * Requires `prettier` to be installed as a peer dependency.
+ */
+export async function formatWithPrettier(content: string, filePath?: string): Promise<string> {
+	let prettier: typeof Prettier
+	try {
+		prettier = await import('prettier')
+	} catch {
+		throw new Error(
+			'The --format flag requires `prettier` to be installed. Run: pnpm add -D prettier',
+		)
+	}
+
+	const config = await prettier.resolveConfig(filePath ?? process.cwd())
+
+	if (config) {
+		log.info(`Using Prettier config for "${filePath ?? process.cwd()}"`)
+	}
+
+	return prettier.format(content, {
+		...config,
+		filepath: filePath,
+		parser: 'markdown',
+	})
+}
