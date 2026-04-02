bfc5770b5c6f955190cd935dd91a3aad3ab6b704
Eric Mika <eric@ericmika.com>
2026-04-02 05:13:31 -0400

    Bring back mdat-plugin-cli-help.

--- mdat.config.ts
+++ mdat.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'mdat'
+import { mdatConfig } from '@kitschpatrol/mdat-config'
 
 // TODO bring these back post 2.0 migration
-// import cliHelpPlugin from 'mdat-plugin-cli-help'
+import cliHelpPlugin from 'mdat-plugin-cli-help'
 // import examplePlugin from 'mdat-plugin-example'
 // import tldrawPlugin from 'mdat-plugin-tldraw'
 
-export default defineConfig({
-	// ...cliHelpPlugin,
+export default mdatConfig({
+	...cliHelpPlugin,
 	// ...examplePlugin,
 	// ...tldrawPlugin,
 })
